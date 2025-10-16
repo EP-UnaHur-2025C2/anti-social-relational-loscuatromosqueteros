75176168e0587fff1218ae5eb81200c44c0ff051
ArielValenzuela1 <arielsebastian.valenzuela@estudiantes.unahur.edu.ar>
2025-10-16 14:48:13 -0300

    cambios en schemas

--- src/middlewares/post.middleware.js
+++ src/middlewares/post.middleware.js
@@ -1,6 +1,17 @@
 const {Post} = require('../db/models');
 const validarByld = require('./generic.middleware');
+const postSchema = require("../schemas/post.schema");
+const genericSchemaValidator = require("../schemas/genericSchemaValidator");
 
-const validarUserById = validarByld(Post);
+const validarPostById = validarByld(Post);
 
-module.exports = {validarUserById};
+const validarSchemaPost = (req,res,next)=>{
+   const {error,_} = genericSchemaValidator(postSchema,req.body)
+   if (error){
+    res.status(400).json({mensaje:"hay errores"});
+    return;
+   }
+   next()
+}
+
+module.exports = {validarPostById, validarSchemaPost};
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const userSchema = Joi.object({
-  nickName: Joi.string().min(3).max(20).required.messages({
+  nickName: Joi.string().min(3).max(20).required().messages({
     "string.empty": "El nombre no puede ser vacío",
     "string.min": "El nombre no puede tener menos de 3 caracteres",
     "string.max":"El nombre tiene que tener como maximo 20 caracteres",
