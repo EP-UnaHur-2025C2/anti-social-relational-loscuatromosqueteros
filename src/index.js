c615318423bd0910ede284b11773b3bfd268cfb4
Franco <francoiannone2@gmail.com>
2025-10-19 03:31:21 -0300

    swagger primer incremento

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ const userRoute= require('./routes/user.route');
 const postRoute= require('./routes/post.route');
 const tagRoute= require('./routes/tag.route');
 
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+ 
+const file  = fs.readFileSync('./src/swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json())
 
 app.use('/user', userRoute);
@@ -23,6 +32,6 @@ app.listen(port, async (err) => {
         console.error('Error starting server: ', err)
         process.exit(1)
     }
-    await db.sync()
+    //await db.sync()
     console.log(`Server is running on port ${port}`)
 })
